a560e0c430837d977dc1ffa423b74f869d0c6a13
Simran Das <simrandas0401@gmail.com>
2022-07-11 02:39:34 +0530

    Implemented the add and delete functionalities in the todo component including its styling

--- todo-list/src/components/Todo.js
+++ todo-list/src/components/Todo.js
@@ -1,14 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import todoImage from "../images/todo.png";
+import "../App.css";
 
 const Todo = () => {
+  const [inputData, setInputData] = useState("");
+  const [items, setItems] = useState([]);
+
+  const addItem = () => {
+    if (inputData) {
+      setItems([...items, inputData]);
+      setInputData("");
+    }
+  };
+
+  const deleteItem = (deletedId) => {
+    const updatedItems = items.filter((item, idx) => {
+      return idx !== deletedId; //filters and returns all the items except the deleted one
+    });
+    setItems(updatedItems);
+  };
+
+  const removeAll = () => {
+    setItems([]);
+  };
+
   return (
     <>
       <div className="main-div">
         <div className="child-div">
           <figure>
             <img src={todoImage} alt="todoLogo" />
+            <figcaption>Todo's 💓</figcaption>
           </figure>
+
+          <div className="addItems">
+            <input
+              type="text"
+              placeholder="Add your todo's here ..."
+              value={inputData}
+              onChange={(e) => setInputData(e.target.value)}
+            />
+            <i className="fa fa-plus" title="Add Item" onClick={addItem}></i>
+          </div>
+
+          <div className="showItems">
+            {items.map((item, idx) => {
+              return (
+                <div className="eachItem" key={idx}>
+                  <h3>{item}</h3>
+                  <i
+                    className="far fa-trash-alt"
+                    title="Delete Item"
+                    onClick={() => deleteItem(idx)}
+                  ></i>
+                </div>
+              );
+            })}
+          </div>
+
+          <div className="showItems">
+            <button
+              className="btn effect04"
+              data-sm-link-text="Remove All"
+              onClick={removeAll}
+            >
+              <span>CHECKLIST</span>
+            </button>
+          </div>
         </div>
       </div>
     </>
