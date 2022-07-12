802efd544371cddf888e60768b485860e7ff0756
Simran Das <simrandas0401@gmail.com>
2022-07-13 02:32:44 +0530

    Added the edit functionality and also implemented local storage save for the items

--- todo-list/src/components/Todo.js
+++ todo-list/src/components/Todo.js
@@ -1,28 +1,72 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import todoImage from "../images/todo.png";
 import "../App.css";
 
+//getting items from local storage after page reload
+const getLocalItems = () => {
+  let list = localStorage.getItem("lists");
+  if (list) {
+    return JSON.parse(localStorage.getItem("lists"));
+  } else {
+    return [];
+  }
+};
+
 const Todo = () => {
   const [inputData, setInputData] = useState("");
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(getLocalItems());
+  const [toggleSubmit, setToggleSubmit] = useState(true);
+  const [editedItem, setEditedItem] = useState(null);
 
   const addItem = () => {
-    if (inputData) {
-      setItems([...items, inputData]);
+    if (!inputData) {
+      alert("Field data required");
+    } else if (inputData && !toggleSubmit) {
+      setItems(
+        items.map((item) => {
+          if (item.id === editedItem) {
+            return { ...item, name: inputData };
+          }
+          return item;
+        })
+      );
+      setToggleSubmit(true);
+      setInputData("");
+      setEditedItem(null);
+    } else {
+      const allInputData = {
+        id: new Date().getTime().toString(),
+        name: inputData,
+      };
+      setItems([...items, allInputData]);
       setInputData("");
     }
   };
 
-  const deleteItem = (deletedId) => {
-    const updatedItems = items.filter((item, idx) => {
-      return idx !== deletedId; //filters and returns all the items except the deleted one
+  const deleteItem = (id) => {
+    const updatedItems = items.filter((item) => {
+      return id !== item.id; //filters and returns all the items except the deleted one
     });
     setItems(updatedItems);
+  };
+
+  const editItem = (id) => {
+    let newlyEditedItem = items.find((item) => {
+      return item.id === id;
+    });
+    setToggleSubmit(false);
+    setInputData(newlyEditedItem.name);
+    setEditedItem(id);
   };
 
   const removeAll = () => {
     setItems([]);
   };
+
+  //adding items to the local storage
+  useEffect(() => {
+    localStorage.setItem("lists", JSON.stringify(items));
+  }, [items]);
 
   return (
     <>
@@ -40,19 +84,34 @@ const Todo = () => {
               value={inputData}
               onChange={(e) => setInputData(e.target.value)}
             />
-            <i className="fa fa-plus" title="Add Item" onClick={addItem}></i>
+            {toggleSubmit ? (
+              <i className="fa fa-plus" title="Add Item" onClick={addItem}></i>
+            ) : (
+              <i
+                className="far fa-edit"
+                title="Update Item"
+                onClick={addItem}
+              ></i>
+            )}
           </div>
 
           <div className="showItems">
-            {items.map((item, idx) => {
+            {items.map((item) => {
               return (
-                <div className="eachItem" key={idx}>
-                  <h3>{item}</h3>
-                  <i
-                    className="far fa-trash-alt"
-                    title="Delete Item"
-                    onClick={() => deleteItem(idx)}
-                  ></i>
+                <div className="eachItem" key={item.id}>
+                  <h3>{item.name}</h3>
+                  <div className="todo-btn">
+                    <i
+                      className="far fa-edit"
+                      title="Edit Item"
+                      onClick={() => editItem(item.id)}
+                    ></i>
+                    <i
+                      className="far fa-trash-alt"
+                      title="Delete Item"
+                      onClick={() => deleteItem(item.id)}
+                    ></i>
+                  </div>
                 </div>
               );
             })}
